78ad958da9e19ad0379a5935246689de15d7b51a
saeid ebrahimi <saeid.ebrahimi127@gmail.com>
2026-06-24 08:32:15 +0330

    feat(db): add file schema and relations

---
+++ src/db/schema/file-schema.ts
@@ -0,0 +1,21 @@
+import { sql } from 'drizzle-orm'
+import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core'
+import { userTable } from './auth-schema'
+
+export const fileTable = pgTable('file', {
+  id: uuid('id')
+    .primaryKey()
+    .default(sql`uuidv7()`),
+  name: text('name').notNull(),
+  userId: uuid('user_id')
+    .notNull()
+    .references(() => userTable.id, { onDelete: 'cascade' }),
+  type: text('type').notNull(),
+  sizeKB: text('size_kb').notNull(),
+  url: text('url').notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at')
+    .defaultNow()
+    .$onUpdate(() => /* @__PURE__ */ new Date())
+    .notNull(),
+})
--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,11 +1,14 @@
 import { relations } from 'drizzle-orm'
 import { accountTable, sessionTable, userTable } from './auth-schema'
+import { fileTable } from './file-schema'
 
 export * from './auth-schema'
+export * from './file-schema'
 
 export const userRelations = relations(userTable, ({ many }) => ({
   sessions: many(sessionTable),
   accounts: many(accountTable),
+  files: many(fileTable),
 }))
 
 export const sessionRelations = relations(sessionTable, ({ one }) => ({
@@ -18,6 +21,13 @@ export const sessionRelations = relations(sessionTable, ({ one }) => ({
 export const accountRelations = relations(accountTable, ({ one }) => ({
   user: one(userTable, {
     fields: [accountTable.userId],
+    references: [userTable.id],
+  }),
+}))
+
+export const fileRelation = relations(fileTable, ({ one }) => ({
+  user: one(userTable, {
+    fields: [fileTable.userId],
     references: [userTable.id],
   }),
 }))
